3fef350ded27abbe4d0448201df14b662e8a19dd
Damian Molinski <tedeer@gmail.com>
2022-05-13 11:55:33 +0200

    Show data and it's process

--- js/index.js
+++ js/index.js
@@ -4,5 +4,22 @@ async function startApp() {
 	const apiUrl = 'http://api.nbp.pl/api/exchangerates/tables/a?format=json';
 	const response = await fetch(apiUrl);
 	const data = await response.json();
-	console.log(data);
+
+	processData(data[0]);
+
+	// console.log(data[0]);
+}
+
+function processData(data) {
+	// console.log(data.rates);
+	console.log(data.rates[0]);
+
+	data.rates.forEach(element => {
+		console.log(element);
+	});
+
+	// console.log(data.table);
+	// console.log(data.no);
+	// const number = document.querySelector('.date');
+	// number.appendChild(data.no);
 }
